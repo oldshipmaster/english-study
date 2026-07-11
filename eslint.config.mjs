13e1e3c9995a4568fc9f691b35bf587991e2b91c
captain <captain.guo@foxmail.com>
2026-07-11 11:02:02 +0800

    chore: ignore isolated worktree artifacts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,9 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".worktrees/**",
+    "coverage/**",
+    "dist/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
